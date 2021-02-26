6b023ec62541837ab73cc9bb226d114eb28496f9
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-02-26 15:37:35 +0900

    [MODIFY] <App> 라우터 수정,

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,26 @@
 import React, { FC } from "react";
-import { Switch, Router, Route, Redirect } from "react-router-dom";
+import { Switch, Router, Route } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-
-import "./lib/confirm/confirm.css";
-import { GlobalStyle, GlobalContainer, GlobalBody } from "./GlobalStyle";
-import { PageNotFound, Navigation } from "./components";
-import {
-  LoginContainer,
-  HeaderContainer,
-  PasswordChangeContainer,
-  ParentContainer
-} from "./containers";
-import {
-  CirclesRouter,
-  NoticeRouter,
-  OutingRouter,
-  MainRouter,
-  ManagementRouter
-} from "./routers";
-import { history } from "./modules/store";
 import { ToastContainer } from "react-toastify";
 
-const App: FC<{}> = () => {
+import "./lib/confirm/confirm.css";
+import { GlobalStyle } from "./GlobalStyle";
+import { Home } from "./components";
+import { ParentContainer } from "./containers";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+const App: FC = () => {
   return (
-    <>
-      {location.pathname.includes("parent") ? (
-        <Router history={history}>
-          <ToastContainer autoClose={2000} />
-          <GlobalStyle />
-          <Switch>
-            <Route path="/parent/:confirmUuid" component={ParentContainer} />
-          </Switch>
-        </Router>
-      ) : (
-        <GlobalContainer>
-          <GlobalStyle />
-          <Router history={history}>
-            <ToastContainer autoClose={2000} />
-            <Navigation />
-            <GlobalBody>
-              <HeaderContainer />
-              <Switch>
-                <Route path="/pw-change" component={PasswordChangeContainer} />
-                <Route path="/login" component={LoginContainer} />
-                <Route path="/home" component={MainRouter} />
-                <Route path="/notice" component={NoticeRouter} />
-                <Route path="/circles" component={CirclesRouter} />
-                <Route path="/outing" component={OutingRouter} />
-                <Route path="/management" component={ManagementRouter} />
-                <Redirect path="/" to="/home" />
-                <Route path="*" component={PageNotFound} />
-              </Switch>
-            </GlobalBody>
-          </Router>
-        </GlobalContainer>
-      )}
-    </>
+    <Router history={history}>
+      <ToastContainer autoClose={2000} />
+      <GlobalStyle />
+      <Switch>
+        <Route path="/:confirmUuid" component={ParentContainer} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 };
 
